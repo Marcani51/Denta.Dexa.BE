e8303bd458e60a0c2aa1f33f4735f017d08433c8
Marcellus Denta Widyapramana <denta.marcellus@gmail.com>
2025-08-11 03:13:25 +0700

    feat: give cors allow

--- auth-service/dist/app.js
+++ auth-service/dist/app.js
@@ -5,10 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.App = void 0;
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const api_v1_1 = require("./api_versioning/api_v1");
 const dotenv_1 = __importDefault(require("dotenv"));
 const app = (0, express_1.default)();
 dotenv_1.default.config();
+app.use((0, cors_1.default)({
+    origin: "http://localhost:5173"
+    //origin:"https://kost-puce.vercel.app"
+}));
 app.use(express_1.default.json());
 app.use("/v1", api_v1_1.api_v1);
 exports.App = app;
--- auth-service/src/app.ts
+++ auth-service/src/app.ts
@@ -1,10 +1,17 @@
 import express from "express";
+import cors from 'cors'
 import { api_v1 } from "./api_versioning/api_v1";
 import doteenv from "dotenv";
 
 const app = express();
 doteenv.config();
 
+app.use(
+    cors({
+      origin: "http://localhost:5173"
+      //origin:"https://kost-puce.vercel.app"
+    })
+  );
 app.use(express.json());
 app.use("/v1", api_v1);
 
