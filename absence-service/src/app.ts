16c3e44512bea3a61cd97a692421be153c8e56a4
Marcellus Denta Widyapramana <denta.marcellus@gmail.com>
2025-08-12 12:58:50 +0700

    feat: change uploads dir

--- absence-service/src/app.ts
+++ absence-service/src/app.ts
@@ -15,7 +15,7 @@ app.use(
   );
 app.use(express.json());
 app.use("/v1", api_v1);
-app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.get("/download/:filename", (req, res) => {
   const filePath = path.join(__dirname, "../uploads", req.params.filename);
   res.download(filePath, (err) => {
